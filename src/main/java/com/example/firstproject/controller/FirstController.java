f1d47395189a316c2a95e723822737b4d5e40e27
hunputer <wlgns3855@gmail.com>
2023-03-20 23:55:33 +0900

    commit

--- src/main/java/com/example/firstproject/controller/FirstController.java
+++ src/main/java/com/example/firstproject/controller/FirstController.java
@@ -1,13 +1,16 @@
 package com.example.firstproject.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class FirstController {
 
     @GetMapping("/hi")
-    public String niceToMeetYou(){
+    public String niceToMeetYou(Model model){
+        model.addAttribute("username", "홍팍");
+
         return "greetings";
     }
 }
